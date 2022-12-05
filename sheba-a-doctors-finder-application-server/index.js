d83ca4dd0bd646bc44e0ffd5cce39ddb46fe34cd
Shakirzaman007 <shakir071096@gmail.com>
2022-12-05 19:02:02 +0600

    connect server with mongodb

--- sheba-a-doctors-finder-application-server/index.js
+++ sheba-a-doctors-finder-application-server/index.js
@@ -1,11 +1,17 @@
 const express = require('express')
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
 const app = express()
-const port = 3000
+const port = process.env.port || 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(cors());
+app.use(express.json());
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+
+const uri = "mongodb+srv://doctors-portal:doctors-portal@cluster0.fdf8ojg.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
