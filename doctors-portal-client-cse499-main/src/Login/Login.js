f4d3a636257e94092814a644085872bc571ccc63
Kamrun Naher Sarna <86709366+KamrunNaher01@users.noreply.github.com>
2022-12-21 01:32:29 +0600

    Update Login.js

--- doctors-portal-client-cse499-main/src/Login/Login.js
+++ doctors-portal-client-cse499-main/src/Login/Login.js
@@ -46,4 +46,85 @@ const Login = () => {
         return <Loading></Loading>
     }
 
-  
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="card w-96 shadow-xl">
+        <div className="card-body">
+
+          <h2 className="text-center text-3xl text-gray-700 font-bold">Login</h2>
+          
+          <form onSubmit={handleSubmit(onSubmit)} className="text-gray-600">
+            
+            {/* Email */}
+          <div className="form-control w-full max-w-xs">
+            <label className="label ">
+                <span className="label-text text-gray-600">Email</span>
+            </label>
+
+            <input type="email" placeholder="Your Email" className="input input-bordered w-full max-w-xs bg-white" 
+                {...register("email", {
+                    pattern: {
+                      value: /^(.+)@(.+)$/,
+                      message: 'enter valid email address'
+                    },
+                    required: {
+                        value: true,
+                        message: 'email is required'
+                    }
+                  })}
+            />
+            
+            <label className="label">
+                {errors.email?.type === 'required' && <span className="label-text-alt text-red-500">{errors?.email?.message}</span>}
+                {errors.email?.type === 'pattern' && <span className="label-text-alt text-red-500">{errors?.email?.message}</span>}
+            </label>
+          </div>
+
+                  {/* Password */}
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+                <span className="label-text text-gray-600">Password</span>
+            </label>
+
+            <input type="password" placeholder="Password" className="input input-bordered w-full max-w-xs bg-white" 
+                {...register("password", {
+                    required: {
+                        value: true,
+                        message: "Password is required"
+                    },
+                    minLength: {
+                        value: 6,
+                        message: "Password must be at least 6 characters"
+                    }
+                }
+                )}
+            />
+            
+            <label className="label">
+                {errors?.password?.type === 'required' && <span className="label-text-alt text-red-500">{errors?.password?.message}</span>}
+                {errors?.password?.type === 'minLength' && <span className="label-text-alt text-red-500">{errors?.password?.message}</span>}
+            </label>
+         </div>
+
+            <input className="btn btn-block" type="submit" value="Login"/>
+        </form>
+        <small className="text-gray-800">New to Doctors Portal? <Link className="text-accent"to="/signup">Create New Account</Link></small>
+
+        {signInError}
+
+          <div className="divider">OR</div>
+
+            {/* Google Log in */}
+          <div className="card-actions justify-end">
+            <button className="btn btn-accent btn-outline btn-block" 
+                onClick={() =>signInWithGoogle()}
+            >Continue with Google</button>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
