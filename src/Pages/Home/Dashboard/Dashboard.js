fa2c596af81f4c03478375edc1b007f98c581dc9
OmarHasanRumi <56462938+OmarHasanRumi@users.noreply.github.com>
2022-12-22 00:26:21 +0600

    Add files via upload

---
+++ src/Pages/Home/Dashboard/Dashboard.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { Link, Outlet } from 'react-router-dom';
+import auth from '../../firebase.init';
+import useAdmin from '../../hooks/useAdmin';
+
+const Dashboard = () => {
+  const [user] = useAuthState(auth);
+  const [admin] = useAdmin(user);
+    return (
+        <div className="drawer drawer-mobile">
+  <input id="dashboard-sidebar" type="checkbox" className="drawer-toggle" />
+  <div className="drawer-content">
+    <h2 className="text-3xl font-bold text-purple-500">Welcome to your Dashboard</h2>
+    <Outlet></Outlet>
+    
+  
+  </div> 
+  <div className="drawer-side">
+    <label htmlFor="dashboard-sidebar" className="drawer-overlay"></label> 
+    <ul className="menu p-4 overflow-y-auto w-48 bg-base-100 text-base-content">
+      
+      <li><Link to="/dashboard/review">My Review</Link></li>
+   
+      
+      {
+        admin && 
+          <>
+            
+            <li><Link to="/dashboard/addDoctor">Add a Doctor</Link></li>
+            <li><Link to="/dashboard/manageDoctor">Manage Doctor</Link></li>
+          </>
+      }
+    </ul>
+  
+  </div>
+</div>
+    );
+};
+
+export default Dashboard;
