229925806b65abacc2843954299bd01ff7112f8c
Kamrun Naher Sarna <86709366+KamrunNaher01@users.noreply.github.com>
2022-12-23 01:44:18 +0600

    Add files via upload

---
+++ doctors-portal-client-cse499-main/src/Login/RequireAuth.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { Navigate, useLocation } from 'react-router-dom';
+import auth from '../../firebase.init';
+import Loading from '../Shared/Loading';
+
+const RequireAuth = ({children}) => {
+
+    let location = useLocation();
+
+    const [user, loading, error] = useAuthState(auth);
+
+    if(loading){
+        return <Loading></Loading>
+    }
+
+    if(!user){
+        return <Navigate to="/login" state={{ from: location }} replace />;
+    }
+
+    return children;
+};
+
+export default RequireAuth;
